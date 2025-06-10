074dae6c6d8089edb76a97f280e97e8cf2677c6c
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-10 23:43:29 +0300

    feat: Update onSearch function

--- src/app/components/user-search/user-search.ts
+++ src/app/components/user-search/user-search.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { WeatherData } from '../../../shared/interfaces/weather-data';
+import { WeatherInfo } from '../weather-info/weather-info';
+import { WeatherService } from '../../services/weather.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-search',
-  imports: [],
+  imports: [FormsModule, WeatherInfo, CommonModule],
   templateUrl: './user-search.html',
   styleUrl: './user-search.css'
 })
 export class UserSearch {
 
+weather: WeatherService = inject(WeatherService);
+
+showWeather = false;
+weatherData?: WeatherData;
+
+
+  cityName: string= "";
+
+  onSearch():void{
+   this.weather.getWeather(this.cityName).subscribe(data => {
+    this.weatherData = data;
+    this.showWeather = true;
+  });
+
+
+}
 }
