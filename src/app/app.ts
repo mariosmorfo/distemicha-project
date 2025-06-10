90707547e2d453ae87b6f133c1f5c71c055c9f38
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-10 19:23:46 +0300

    feat: Import UserSearch and WeatherInfo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserSearch } from "./components/user-search/user-search";
+import { WeatherInfo } from "./components/weather-info/weather-info";
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,  
+  imports: [RouterOutlet, UserSearch, WeatherInfo ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
