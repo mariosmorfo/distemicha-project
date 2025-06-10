4547424f1748ab07e9acf10069384080acbe33e5
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-10 19:25:22 +0300

    feat: Import provideHttpClient

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient } from '@angular/common/http';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideHttpClient(),
+    ...appConfig.providers
+  ]
+});
