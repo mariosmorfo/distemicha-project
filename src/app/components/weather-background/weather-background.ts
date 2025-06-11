0663e8abb0a1843cdd47dc6d0c96f4ce7f5471dc
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-11 11:56:02 +0300

    feat: Create weatherBackground function for changing the color

--- src/app/components/weather-background/weather-background.ts
+++ src/app/components/weather-background/weather-background.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-weather-background',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './weather-background.html',
   styleUrl: './weather-background.css'
 })
 export class WeatherBackground {
+
+  @Input() isDay: undefined | boolean = true
+  @Input() weatherDescription : undefined | string = ""
+
+  get weatherBackground(): string {
+    if(!this.isDay){
+      return 'night';
+    }
+
+    if(this.weatherDescription?.toLocaleLowerCase().includes('rain')){
+      return 'rainy';
+    }
+
+    if(this.weatherDescription?.toLocaleLowerCase().includes('cloud')){
+      return 'cloudy'
+    }
+
+    if(this.weatherDescription?.toLocaleLowerCase().includes('sun') || this.weatherDescription?.toLocaleLowerCase().includes('clear')){
+      return 'sunny'
+    }
+
+    return 'default-day'
+  }
+
 
 }
