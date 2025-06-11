6018dac726a7ee3fa69e0ea00403eba718b461cc
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-11 15:58:16 +0300

    feat: Add a new EventEmitter for FavoriteClick function

--- src/app/components/weather-info/weather-info.ts
+++ src/app/components/weather-info/weather-info.ts
@@ -11,11 +11,17 @@ import { NgIcon } from '@ng-icons/core';
 export class WeatherInfo {
 
   @Input() weather?: WeatherData
-
   @Output() back = new EventEmitter<void>();
+  @Output() favorite = new EventEmitter<string>();
 
-  onBackClick(){
+  onBackClick(): void{
    this.back.emit();
+  }
+
+  onFavoriteClick(): void{
+    if(this.weather?.cityName){
+      this.favorite.emit(this.weather.cityName)
+    }
   }
   
   
