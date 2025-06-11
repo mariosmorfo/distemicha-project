e0738b3f6677b013342df7f3752d1c1bb50b96ca
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-06-11 14:11:00 +0300

    feat: Import ngIcons

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserSearch } from "./components/user-search/user-search";
 import { WeatherInfo } from "./components/weather-info/weather-info";
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { featherArrowLeft } from '@ng-icons/feather-icons';
+import { bootstrapBalloonHeart } from '@ng-icons/bootstrap-icons';
+
+
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,  
-  imports: [RouterOutlet, UserSearch, WeatherInfo ],
+  imports: [RouterOutlet, UserSearch, WeatherInfo, NgIcon],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+   viewProviders: [provideIcons({featherArrowLeft, bootstrapBalloonHeart  })]
 })
 export class App {
   protected title = 'distemicha-project';
